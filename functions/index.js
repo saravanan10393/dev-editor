9d75ef4fdc1087c323511638bb26c4c6206bab9f
saravanan10393 <saravanan.10393@gmail.com>
2019-09-16 11:09:04 +0530

    logs added

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.perfTest = functions.https.onRequest((request, response) => {
-  console.log("======= Request header =====", request.headers);
   console.log("======= Request body =====", request.body);
+  console.log("======= Request itself =====", request);
   response.send("Hello from Firebase!");
 });
